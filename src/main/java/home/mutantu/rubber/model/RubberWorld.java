04569396d121ab581c11a5eebbba5d428c442020
mutantu2002 <mutantu2002@gmail.com>
2012-09-18 12:48:49 +0000

    First working revision

--- src/main/java/home/mutantu/rubber/model/RubberWorld.java
+++ src/main/java/home/mutantu/rubber/model/RubberWorld.java
@@ -1,11 +1,13 @@
 package home.mutantu.rubber.model;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class RubberWorld
 {
 	List<RubberObject> objects = new ArrayList<RubberObject>();
+
 	public void next()
 	{
 		for (RubberObject obj : objects) 
@@ -13,5 +15,22 @@ public class RubberWorld
 			obj.next(this);
 		}
 	}
+	
+	public List<RubberObject> getObjects()
+	{
+		return Collections.unmodifiableList(objects);
+	}
 
+	public void addObject(RubberObject obj)
+	{
+		objects.add(obj);
+	}
+
+	public void flipCoordinates()
+	{
+		for (RubberObject obj : objects) 
+		{
+			obj.flipCoordinates();
+		}		
+	}
 }
