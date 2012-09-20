071a260c21ef7f50d56d23d6b517155d5bdd61fe
mutantu2002@gmail.com <mutantu2002@gmail.com@6982fbd5-05bb-cd05-f27b-9b42db1acb05>
2012-09-20 20:53:37 +0000

    Some minor correction

--- trunk/src/main/java/home/mutantu/rubber/model/Constants.java
+++ trunk/src/main/java/home/mutantu/rubber/model/Constants.java
@@ -2,11 +2,13 @@ package home.mutantu.rubber.model;
 
 public class Constants
 {
-	public static final double FRICTION = 0.0001;
-	public static final double GRAVITY = 0.9;
-	public static final double ELASTIC_CONSTANTS=6;
+	public static final double FRICTION = 0.001;
+	public static final double GRAVITY = 1.9;
+	public static final double ELASTIC_CONSTANTS=7;
 	public static final double DT=0.12;
-	public static final int WIDTH = 800;
-	public static final int HEIGHT = 600;
+	public static final int WIDTH = 600;
+	public static final int HEIGHT = 480;
 	public static final double EPSILON = 0.001;
+	
+	public static final int PAUSE_MILISEC = 2;
 }
