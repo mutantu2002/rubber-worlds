117bbfc888e7fc2a9573cdc2775c318726686dd5
mutantu2002 <mutantu2002@gmail.com>
2012-09-20 10:03:59 +0000

    Add round objects

--- src/main/java/home/mutantu/rubber/Main.java
+++ src/main/java/home/mutantu/rubber/Main.java
@@ -2,12 +2,14 @@ package home.mutantu.rubber;
 
 import home.mutantu.rubber.controller.WorldFactory;
 import home.mutantu.rubber.controller.WorldRunner;
+import home.mutantu.rubber.model.Constants;
 import home.mutantu.rubber.ui.WorldFrame;
 
 public class Main
 {
 	public static void main(String[] args)
 	{
-		new Thread(new WorldRunner(WorldFactory.createOneRectangleObjectWorld(200,50,15,13,13), new WorldFrame(800,600))).start();
+		//new Thread(new WorldRunner(WorldFactory.createOneRectangleObjectWorld(200,50,15,13), new WorldFrame(Constants.WIDTH,Constants.HEIGHT))).start();
+		new Thread(new WorldRunner(WorldFactory.createOneRoundObjectWorld(400,300,21,10), new WorldFrame(Constants.WIDTH,Constants.HEIGHT))).start();
 	}
 }
